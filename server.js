20f31a31c799faf4c323cea4cfc42a18d8600bb7
Gagan Tayal <gagantayal2003@gmail.com>
2025-03-21 21:34:26 +0530

    fulli

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use('/api/events', eventRoutes);
-app.use('/api/auth', authRoutes);
+
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
